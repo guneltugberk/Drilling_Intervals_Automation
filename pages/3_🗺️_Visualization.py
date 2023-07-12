107a8930cb7a6d54cbd7f95c7e1e0d50a7ee0928
Tuğberk Günel <70752223+guneltugberk@users.noreply.github.com>
2023-07-12 16:57:27 +0300

    Update 3_🗺️_Visualization.py

--- pages/3_🗺️_Visualization.py
+++ pages/3_🗺️_Visualization.py
@@ -151,7 +151,7 @@ def main():
                 st.plotly_chart(figure)
 
                 st.table(data=st.session_state.df_numeric_stats)
-                st.write(session_state.copy_df_stats.columns)
+                st.write(st.session_state.copy_df_stats.columns)
 
 
             elif visualization_type == 'Feature Investigation' and data_type == 'Use Prior Data':
