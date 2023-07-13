0f775fab6dd567d31ae86d541e1f9f3c2389cc33
Tuğberk Günel <70752223+guneltugberk@users.noreply.github.com>
2023-07-13 16:06:57 +0300

    Update 2_📐_Intervals_Automation.py

--- pages/2_📐_Intervals_Automation.py
+++ pages/2_📐_Intervals_Automation.py
@@ -478,29 +478,26 @@ def main():
                 st.table(data=st.session_state.prior_data.describe())
                 st.divider()
 
-                try:
-                    
-                    prior_data_rocks = Intervals.Formations(st.session_state.prior_data, depth_intervals, formations, 'prior data')
-                    stats_data_rocks = Intervals.Formations(st.session_state.stats_data, depth_intervals, formations, 'stats data')
-                    st.write('Girdim')
-                    if 'prior_data_rocks' not in st.session_state:
-                        st.session_state.prior_data_rocks = prior_data_rocks
+                prior_data_rocks = Intervals.Formations(st.session_state.prior_data, depth_intervals, formations, 'prior data')
+                stats_data_rocks = Intervals.Formations(st.session_state.stats_data, depth_intervals, formations, 'stats data')
+           
+                if 'prior_data_rocks' not in st.session_state:
+                    st.session_state.prior_data_rocks = prior_data_rocks
 
-                    if 'stats_data_rocks' not in st.session_state:
-                        st.session_state.stats_data_rocks = stats_data_rocks
+                if 'stats_data_rocks' not in st.session_state:
+                    st.session_state.stats_data_rocks = stats_data_rocks
 
-                    st.markdown(f"""
-                    <div class='stHeader'><i>{file_name_without_extension}</i> Intervals Data</div>
-                    """, unsafe_allow_html=True)
+                st.markdown(f"""
+                <div class='stHeader'><i>{file_name_without_extension}</i> Intervals Data</div>
+                """, unsafe_allow_html=True)
 
-                    st.table(data=st.session_state.stats_data_rocks)
-                    st.caption(f'**Calculated Number of Intervals:** {counted_interval}')
-                    st.caption(f'**Number of Intervals in Dataset:** {intervals}')
+                st.table(data=st.session_state.stats_data_rocks)
+                st.caption(f'**Calculated Number of Intervals:** {counted_interval}')
+                st.caption(f'**Number of Intervals in Dataset:** {intervals}')
 
-                    display_chart = True
+                display_chart = True
 
-                except:
-                    st.error('According to the algorithm, the dataset is found to be not convenient.')
+
 
             elif st.session_state.flag == 0:
                 st.warning('Please do not forget to select the following columns: *Zeit*, *Teufe*, *Delta Zeit*', icon='💹')
